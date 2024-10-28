2f151326a16d05d402a170d6c81ec753723cf7ca
Diego Benjumea <diegobenjumeafreelance@outlook.com>
2024-10-28 14:18:26 -0500

    1.0.000: Creación de la estructura inical del proyecto

---
+++ Backend/User/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios al contenedor.
+builder.Services.AddControllers();
+
+// Configurar Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configurar el uso de Swagger en entorno de desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ SECURITY.md
@@ -0,0 +1,12 @@
+# Security policy
+
+## Supported Versions
+
+| Version | Supported          |
+| ------- | ------------------ |
+| 1.x     | :white_check_mark: |
+| 1.1     | :white_check_mark: |
+| 0.x     | :x:                |
+
+## Reporting a Vulnerability
+If you discover a security vulnerability in this library, please report it to the [security email address]. We will addres all security concerns with the highest priority.
